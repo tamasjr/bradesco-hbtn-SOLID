bf1c9c67b24265eb4991f99cfd123f4d54476dbd
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-10-16 00:44:53 -0300

    Convenções de Nomenclatura

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,19 @@
+public class UserModel {
+
+    private int idUser;
+    private String descricao;
+
+    public static final int IDADE_MINIMA = 18;
+
+    public List<User> fetchUsers() {
+        return new ArrayList<>();
+    }
+
+    public List<User> getAllUsers() {
+        return new ArrayList<>();
+    }
+
+    public List<User> getUsers() {
+        return new ArrayList<>();
+    }
+}
