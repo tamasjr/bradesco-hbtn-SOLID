60b9cae7721e2b376b1fe137c43f3e83ede87f60
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-10-16 00:28:57 -0300

    Update Comments.java

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -12,7 +12,8 @@ public class Comments {
     }
 
     /**
-     * <p>Este método publica resultados com base na data.</p>
+     * <p>Este método publica resultados com base na data
+     * </p>
      * @param date Data a partir da qual os resultados devem ser publicados
      * @return Lista de entidades de resultado
      * @since 16.0
