c472a8e0c602390147e98ccc7695ffc79a143515
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-10-16 00:26:54 -0300

    Comentarios

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,22 @@
+public class Comments {
+
+    public Owner getTestcaseOwner(Testcase testcase) {
+    }
+
+    public Hours getHoursInQuarter() {
+    }
+
+    // TODO Este método deve ser removido
+    public void check() {
+        return;
+    }
+
+    /**
+     * <p>Este método publica resultados com base na data.</p>
+     * @param date Data a partir da qual os resultados devem ser publicados
+     * @return Lista de entidades de resultado
+     * @since 16.0
+     */
+    public List<Result> publishResults(Date date) {
+    }
+}
