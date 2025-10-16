865c0deb08d8003a0fe25bca0c763b069f232b2a
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-10-16 00:36:54 -0300

    Update Comments.java

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -1,5 +1,8 @@
 public class Comments {
 
+    // Copyright (C) 2022 by CleanCode.
+    // Lançado sob os termos da GNU General Public License versão 2 ou posterior
+
     public Owner getTestcaseOwner(Testcase testcase) {
     }
 
